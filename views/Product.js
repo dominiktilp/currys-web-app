e8e3ae3ea84db596899d7ff267047380f58bfe61
Dominik Tilp <Dominik.Tilp@DixonsCarphone.com>
2016-06-02 23:22:41 +0200

    merge

--- views/Product.js
+++ views/Product.js
@@ -26,6 +26,7 @@ class Product extends React.Component {
     }
   }
 
+<<<<<<< Updated upstream
   render() {
     return (
       <div>
@@ -33,6 +34,56 @@ class Product extends React.Component {
 
       </div>
     );
+=======
+  rawDescription(description) {
+    return { __html: description };
+  }
+
+  renderFeature(item) {
+    return (
+      <li>{item}</li>
+    );
+  }
+
+  render() {
+    if (!this.props.state.getIn(['app', 'product'])) {
+      return (
+        <div className="appLoader">
+        ...loading...
+        </div>
+      );
+    } else {
+      const product = this.props.state.getIn(['app', 'product']).toJS();
+      let paragraphs = product.description.split("<br /><br />");
+      const firstParagraph = paragraphs.pop();
+      const restDesc = paragraphs.join("<br /><br />");
+      return (
+        <div>
+          <div className="productImage">
+            <img src={product.images[0]} />
+          </div>
+          <div className="productInfo">
+            <h1>{product.name}</h1>
+            <div className="productId">
+            {product.id}
+            </div>
+            <div className="productPrice">
+            £{Math.round(product.price, 2)}
+            </div>
+          </div>
+          <div className="productDescription firstParagraph" dangerouslySetInnerHTML={this.rawDescription(firstParagraph)}>
+          </div>
+          <div className="productFeatures">
+            <ul>
+              {product.features.map(this.renderFeature)}
+            </ul>
+          </div>
+          <div className="productDescription" dangerouslySetInnerHTML={this.rawDescription(restDesc)}>
+          </div>
+        </div>
+      );
+    };
+>>>>>>> Stashed changes
   }
 
 }
